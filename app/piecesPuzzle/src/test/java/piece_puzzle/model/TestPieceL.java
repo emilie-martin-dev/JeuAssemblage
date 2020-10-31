0c95c1ea8ab077842e8149077ae2fdeb73c40262
Justine Martin <justine.martin.dev@gmail.com>
2020-10-31 10:28:54 +0100

    chore : Test unitaire lors d'un push / pull_request sur GitHub

---
+++ app/piecesPuzzle/src/test/java/piece_puzzle/model/TestPieceL.java
@@ -0,0 +1,16 @@
+package piece_puzzle.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestPieceL {
+	
+	@Test
+	public void generate() {
+		Assert.assertTrue(true);
+	}
+	
+}
